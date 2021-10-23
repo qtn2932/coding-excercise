d1562988560d14641802bc3117c4fdb3478a49e9
Quan Nguyen <quan.nguyen199712@gmail.com>
2021-10-22 21:27:50 -0400

    remove empty string from href

--- exercise/src/layout/NavBar.js
+++ exercise/src/layout/NavBar.js
@@ -3,7 +3,7 @@ import {React, Fragment} from 'react'
 const NavBar = ()=>(
     <Fragment>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a href="" className="navbar-brand col-sm-8 col-md-2 mr-0 align-items-center">Huskies</a>
+            <a href className="navbar-brand col-sm-8 col-md-2 mr-0 align-items-center">Huskies</a>
         </nav>
     </Fragment>
 )
